c7dd60a77c79abff7e922abb0a6f29c00358c2d2
Alex <maryasov.oleksiy@gmail.com>
2018-09-29 16:27:16 +0300

    2 java-files

---
+++ src/ua/lviv/lgs/jb2/HelloWorld1.java
@@ -0,0 +1,7 @@
+package ua.lviv.lgs.jb2;
+
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Helle World 1!");
+    }
+}
---
+++ src/ua/lviv/lgs/jb2/HelloWorld2.java
@@ -0,0 +1,7 @@
+package ua.lviv.lgs.jb2;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Helle World 2!");
+    }
+}
